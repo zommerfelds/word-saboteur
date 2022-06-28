296cd03ff5a36227eead761e41084a67e8e9fe94
Christian Zommerfelds <christian.zommerfelds@gmail.com>
2022-06-28 21:05:59 +0000

    Allow unfocussing from text field on mobile

--- src/hx/game/Gui.hx
+++ src/hx/game/Gui.hx
@@ -1,6 +1,7 @@
 package game;
 
 class TextInputWithMobileKeyboardSupport extends h2d.TextInput {
+	// Hidden input element used for mobile devices to toggle the onscreen keyboard.
 	final inputElem:js.html.InputElement = cast js.Browser.document.getElementById("dummyInput");
 
 	public function new(font, parent) {
@@ -11,10 +12,11 @@ class TextInputWithMobileKeyboardSupport extends h2d.TextInput {
 		// Clear the text in case a previous value was entered.
 		inputElem.value = "";
 
+		// This is only useful for mobile, but maybe we could enable it always just to avoid branching?
 		if (hxd.System.getValue(IsTouch)) {
 			onClick = (e) -> {
 				inputElem.focus();
-                inputElem.setSelectionRange(cursorIndex, cursorIndex);
+				inputElem.setSelectionRange(cursorIndex, cursorIndex);
 			};
 		}
 	}
@@ -22,27 +24,30 @@ class TextInputWithMobileKeyboardSupport extends h2d.TextInput {
 	override function sync(ctx) {
 		if (hxd.System.getValue(IsTouch)) {
 			text = inputElem.value;
-			cursorIndex = inputElem.selectionStart;
-            selectionSize = 1;
+			if (js.Browser.document.activeElement == inputElem) {
+				cursorIndex = inputElem.selectionStart;
+			} else {
+				cursorIndex = -1;
+			}
 		}
-        super.sync(ctx);
+		super.sync(ctx);
 	}
 }
 
 class Button extends h2d.Flow {
-    public function new(parent, text, onClick) {
-        super(parent);
+	public function new(parent, text, onClick) {
+		super(parent);
 		backgroundTile = h2d.Tile.fromColor(0x77777);
 		padding = 30;
 		verticalAlign = Middle;
 		horizontalAlign = Middle;
 		enableInteractive = true;
-        fillWidth = true;
+		fillWidth = true;
 		interactive.onClick = (e) -> onClick();
 
 		final buttonText = new h2d.Text(hxd.res.DefaultFont.get(), this);
 		buttonText.text = text;
 		buttonText.scale(2);
 		paddingBottom += Std.int(buttonText.getBounds().height * 0.3);
-    }
+	}
 }
